e35ca910b7dc383ed57e46e86149a5893f4d6329
Dennis <driverajr127@gmail.com>
2022-11-15 21:16:38 -0500

    Deleted Intro compoent. Added Navbar component. Introduced headlessui and heroicons.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,9 @@
 import Head from 'next/head'
 import { GetStaticProps } from 'next'
+import Navbar from '../components/navbar'
 import Container from '../components/container'
 import MoreStories from '../components/more-stories'
 import HeroPost from '../components/hero-post'
-import Intro from '../components/intro'
 import Layout from '../components/layout'
 import { getAllPostsForHome } from '../lib/api'
 import { CMS_NAME } from '../lib/constants'
@@ -17,8 +17,8 @@ export default function Index({ allPosts: { edges }, preview }) {
       <Head>
         <title>Next.js Blog Example with {CMS_NAME}</title>
       </Head>
+      <Navbar />
       <Container>
-        <Intro />
         {heroPost && (
           <HeroPost
             title={heroPost.title}
