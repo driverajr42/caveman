e6918125b56c19d4a1bb6a88d97cbc76db753bca
Dennis <driverajr127@gmail.com>
2022-11-24 18:45:40 -0500

    Minor error changes fixed.

--- components/post-header.tsx
+++ components/post-header.tsx
@@ -15,15 +15,12 @@ export default function PostHeader({
   return (
     <>
       <PostTitle>{title}</PostTitle>
-      <div className="hidden md:block md:mb-12">
-        <Avatar author={author} />
-      </div>
       <div className=" mb-8 md:mb-16 sm:mx-0">
         <CoverImage title={title} coverImage={coverImage} />
       </div>
       <SectionSeparator />
-      <div className="max-w-2xl mx-auto">
-        <div className="block md:hidden mb-6">
+      <div className="max-w-2xl mx-auto px-5">
+        <div className="block mb-6">
           <Avatar author={author} />
         </div>
         <div className="mb-6 text-lg">
