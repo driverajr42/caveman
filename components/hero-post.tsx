de5d0093266e26a01cf329544eebc60cd75f2f87
Dennis <106558555+driverajr42@users.noreply.github.com>
2022-12-02 07:14:09 -0800

    Imported google fonts to index.css file.

--- components/hero-post.tsx
+++ components/hero-post.tsx
@@ -1,41 +1,34 @@
-import Avatar from './avatar'
-import Date from './date'
-import CoverImage from './cover-image'
-import Link from 'next/link'
+import Link from 'next/link';
+import Avatar from './avatar';
+import CoverImage from './cover-image';
+import Date from './date';
 
-export default function HeroPost({
-  title,
-  coverImage,
-  excerpt,
-  slug,
-}) {
-  return (
-    <section className='relative top-32'>
-      <div className="mb-8 md:mb-16">
-        {coverImage && (
-          <CoverImage title={title} coverImage={coverImage} slug={slug} />
-        )}
-      </div>
-      <div className="md:grid md:grid-cols-2 md:gap-x-16 lg:gap-x-8 mb-20 md:mb-28">
-        <div className='text-center'>
-          <h3 className="mb-4 text-4xl lg:text-6xl leading-tight">
-            <Link
-              href={`/posts/${slug}`}
-              className="hover:underline"
-              dangerouslySetInnerHTML={{ __html: title }}
-            ></Link>
-          </h3>
-          
-        </div>
-        <div>
-          <div
-            className="text-lg text-center leading-relaxed mb-4"
-            dangerouslySetInnerHTML={{ __html: excerpt }}
-          />
-      
-        </div>
-        <div className='border-b-accent-7 border-b-2 mt-8'></div>
-      </div>
-    </section>
-  )
+export default function HeroPost({ title, coverImage, excerpt, slug }) {
+	return (
+		<section className="relative top-32">
+			<div className="mb-8 md:mb-16">
+				{coverImage && (
+					<CoverImage title={title} coverImage={coverImage} slug={slug} />
+				)}
+			</div>
+			<div className="md:grid md:grid-cols-2 md:gap-x-16 lg:gap-x-8 mb-20 md:mb-28">
+				<div className="text-center">
+					<h3 className="mb-4 text-4xl lg:text-6xl leading-tight font-['Rokkitt','serif']">
+						<Link
+							href={`/posts/${slug}`}
+							className="hover:underline"
+							dangerouslySetInnerHTML={{ __html: title }}
+						></Link>
+					</h3>
+				</div>
+				<div>
+					<div
+						className="text-lg text-center leading-relaxed mb-4 font-['Roboto', 'sans-serif']"
+						dangerouslySetInnerHTML={{ __html: excerpt }}
+					/>
+				</div>
+				<div className="border-b-accent-7 border-b-2 mt-8"></div>
+			</div>
+		</section>
+	);
 }
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,94 +1,100 @@
-import React, { useState, useEffect} from 'react'
-import Link from 'next/link'
-import Image from 'next/image'
-import { AiOutlineMenu, AiOutlineClose } from 'react-icons/ai'
+import Image from 'next/image';
+import Link from 'next/link';
+import React, { useEffect, useState } from 'react';
+import { AiOutlineClose, AiOutlineMenu } from 'react-icons/ai';
 
-import Logo from '../docs/logo.svg'
+import Logo from '../docs/logo.svg';
 
 const Navbar = () => {
+	const [nav, setNav] = useState(false);
 
-    const [nav, setNav ] = useState(false)
+	// These states are to change the background and text colors when user scrolls down
+	const [color, setColor] = useState('transparent');
+	const [textColor, setTextColor] = useState('white');
 
-    // These states are to change the background and text colors when user scrolls down
-    const [color, setColor] = useState('transparent')
-    const [textColor, setTextColor] = useState('white')
+	const handleNav = () => {
+		setNav(!nav);
+	};
 
-    const handleNav = () => {
-        setNav(!nav)
-    } 
+	useEffect(() => {
+		// If user scrolled down the window passed a certain point the background and text color of navbar will change using the following effect
+		const colorChange = () => {
+			if (window.scrollY >= 90) {
+				setColor('#fff');
+				setTextColor('#000');
+			} else {
+				setColor('transparent');
+				setTextColor('#fff');
+			} // Else if user is not scrolled down a certain piont than effect will return back to its orignal state
+		};
+		// Telling the window to listen to the scroll
+		window.addEventListener('scroll', colorChange);
+	}, []);
 
+	return (
+		<div
+			style={{ backgroundColor: `${color}` }}
+			className="fixed left-0 top-0 w-full h-24 z-10 mb-12 ease-in duration-300"
+		>
+			{/* Company Logo */}
+			<div className="max-w-[1240px]  flex justify-between items-center p-4 text-white">
+				<h1 className="text-accent-2 text-2xl font-bold font-['Rokkitt']">
+					Thoughts <br />
+					of a Caveman
+				</h1>
 
-    useEffect(()=>{
-        // If user scrolled down the window passed a certain point the background and text color of navbar will change using the following effect
-        const colorChange = () => {
-            if(window.scrollY >= 90 ) {
-                setColor('#fff')
-                setTextColor('#000')
-            } else {
-                setColor('transparent')
-                setTextColor('#fff')
-            } // Else if user is not scrolled down a certain piont than effect will return back to its orignal state
-        }
-        // Telling the window to listen to the scroll
-        window.addEventListener('scroll', colorChange)
-    }, [])
+				<ul style={{ color: `${textColor}` }} className="hidden sm:flex">
+					<li className="p-4">
+						<Link href="/">Home</Link>
+					</li>
+					<li className="p-4">
+						<Link href="/#gallery">Gallery</Link>
+					</li>
+					<li className="p-4">
+						<Link href="/portfolio">Work</Link>
+					</li>
+					<li className="p-4">
+						<Link href="/contact">Contact</Link>
+					</li>
+				</ul>
 
-  return (
-    <div style={{backgroundColor:`${color}` }} className='fixed left-0 top-0 w-full h-24 z-10 mb-12 ease-in duration-300'>
-        {/* Company Logo */}
-        <div className='max-w-[1240px] -mt-16 flex justify-between items-center p-4 text-white'>
-            <Link href='/'>
-            <Image
-      src={Logo}
-      alt="Picture of the author"
-      width={200}
-      height={200}
-    />
-            </Link>
+				{/* Mobile Menu Button */}
+				<div onClick={handleNav} className="block sm:hidden z-10">
+					{/* If navbar is open show the close icon. Else show the menu icon */}
+					{nav ? (
+						<AiOutlineClose size={20} className="text-accent-1" />
+					) : (
+						<AiOutlineMenu size={20} className="text-accent-2" />
+					)}
+				</div>
 
-            <ul style={{color:`${textColor}` }} className='hidden sm:flex'>
-                <li className='p-4'>
-                    <Link href='/'>Home</Link>
-                </li>
-                <li className='p-4'>
-                    <Link href='/#gallery'>Gallery</Link>
-                </li>
-                <li className='p-4'>
-                    <Link href='/portfolio'>Work</Link>
-                </li>
-                <li className='p-4'>
-                    <Link href='/contact'>Contact</Link>
-                </li>   
-            </ul>
+				{/* Mobile Menu */}
+				{/* If nav is not showing display the first styles. If it is open the show the second */}
+				<div
+					className={
+						nav
+							? 'sm:hidden absolute top-0 left-0 right-0 bottom-0 flex justify-center items-center w-full h-screen bg-accent-2 text-center ease-in duration-300'
+							: 'sm:hidden absolute top-[100vh] left-0 right-0 bottom-0 flex justify-center items-center w-full h-screen bg-black text-center ease-in duration-300'
+					}
+				>
+					<ul className="font-['Roboto','sans-serif']">
+						<li className="p-4 text-4xl hover:text-gray-500">
+							<Link href="/">Home</Link>
+						</li>
+						<li className="p-4 text-4xl hover:text-gray-500">
+							<Link href="/#gallery">Gallery</Link>
+						</li>
+						<li className="p-4 text-4xl hover:text-gray-500">
+							<Link href="/portfolio">Work</Link>
+						</li>
+						<li className="p-4 text-4xl hover:text-gray-500">
+							<Link href="/contact">Contact</Link>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	);
+};
 
-            {/* Mobile Menu Button */}
-            <div onClick={handleNav} className='block sm:hidden z-10'>
-                {/* If navbar is open show the close icon. Else show the menu icon */}
-                { nav ? <AiOutlineClose size={20} className='text-accent-1'/> : <AiOutlineMenu size={20} className='text-accent-2'/>}
-            </div>
-
-            {/* Mobile Menu */}
-            {/* If nav is not showing display the first styles. If it is open the show the second */}
-            <div className={nav ? 'sm:hidden absolute top-0 left-0 right-0 bottom-0 flex justify-center items-center w-full h-screen bg-accent-2 text-center ease-in duration-300' : 'sm:hidden absolute top-[100vh] left-0 right-0 bottom-0 flex justify-center items-center w-full h-screen bg-black text-center ease-in duration-300'}>
-            <ul>
-                <li className='p-4 text-4xl hover:text-gray-500'>
-                    <Link href='/'>Home</Link>
-                </li>
-                <li className='p-4 text-4xl hover:text-gray-500'>
-                    <Link href='/#gallery'>Gallery</Link>
-                </li>
-                <li className='p-4 text-4xl hover:text-gray-500'>
-                    <Link href='/portfolio'>Work</Link>
-                </li>
-                <li className='p-4 text-4xl hover:text-gray-500'>
-                    <Link href='/contact'>Contact</Link>
-                </li>   
-            </ul>
-            </div>
-
-        </div>
-    </div>
-  )
-}
-
-export default Navbar
+export default Navbar;
