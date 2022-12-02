95f63b366ed2ad8e4aad0042e8ea2251cc05405c
Dennis <106558555+driverajr42@users.noreply.github.com>
2022-12-02 12:15:37 -0800

    In Footer component, added social icons and added copyright info.

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,30 +1,29 @@
-
-import { EXAMPLE_PATH } from '../lib/constants'
-
+import { AiFillInstagram, AiFillTwitterCircle } from 'react-icons/ai';
+import { EXAMPLE_PATH } from '../lib/constants';
 export default function Footer() {
-  return (
-    <footer className="bg-accent-1 border-t border-accent-2">
-      <div>
-        <div className="py-28 flex flex-col lg:flex-row items-center">
-          <h3 className="text-4xl lg:text-5xl font-bold tracking-tighter leading-tight text-center lg:text-left mb-10 lg:mb-0 lg:pr-4 lg:w-1/2">
-            Statically Generated with Next.js.
-          </h3>
-          <div className="flex flex-col lg:flex-row justify-center items-center lg:pl-4 lg:w-1/2">
-            <a
-              href="https://nextjs.org/docs/basic-features/pages"
-              className="mx-3 bg-black hover:bg-white hover:text-black border border-black text-white font-bold py-3 px-12 lg:px-8 duration-200 transition-colors mb-6 lg:mb-0"
-            >
-              Read Documentation
-            </a>
-            <a
-              href={`https://github.com/vercel/next.js/tree/canary/examples/${EXAMPLE_PATH}`}
-              className="mx-3 font-bold hover:underline"
-            >
-              View on GitHub
-            </a>
-          </div>
-        </div>
-      </div>
-    </footer>
-  )
+	return (
+		<footer className=" bg-accent-2 text-accent-1">
+			<div>
+				<div className="py-28 flex flex-col lg:flex-row items-center">
+					<h3 className="text-4xl lg:text-5xl font-bold font-['Crimson-Text'] tracking-tight leading-tight text-center lg:text-left mb-10 lg:mb-0 lg:pr-4 lg:w-1/2">
+						Thoughts of a Caveman
+					</h3>
+
+					{/* Soical Media Icons */}
+					<div className="flex flex-row justify-evenly items-center lg:pl-4 lg:w-1/2">
+						<a href="/" className="ml-3">
+							<AiFillTwitterCircle size={40} />
+						</a>
+						<a href="/" className="ml-3">
+							<AiFillInstagram size={40} />
+						</a>
+					</div>
+					{/* Copyright Info */}
+					<p className="text-accent-1 text-center mt-12">
+						&copy; 2022-2023 Jungo Writes. <br /> All rights reserved.
+					</p>
+				</div>
+			</div>
+		</footer>
+	);
 }
